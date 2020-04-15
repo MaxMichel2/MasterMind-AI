6207781a347ef0e865b2466845d1ab1645d246ae
MaxMichel2 <maxime_michel_uk@hotmail.com>
2020-04-15 11:31:18 +0200

    Initial commit

---
+++ Mastermind-AI.py
@@ -0,0 +1,37 @@
+import random
+import math
+import sys
+
+#########################################
+############# MasterMind AI #############
+#########################################
+
+#########################################
+################ Step 1 #################
+##### Model the MasterMind problem ######
+#########################################
+
+
+# Question 1 #
+# Propose a representation (coding) for the pawn combinations
+
+# Question 2 #
+# How many candidate solutions are there for a combination of N pawns of k colours ?
+# Deduce the number of combinations for 4 pawns with 8 colours
+
+# Question 3 #
+
+# Question 3.1 #
+
+# Question 3.2 #
+
+#########################################
+################ Step 2 #################
+##### Selection, Crossing, Mutation #####
+#########################################
+
+# Question 1 #
+
+# Question 2 #
+
+# Question 3 #
